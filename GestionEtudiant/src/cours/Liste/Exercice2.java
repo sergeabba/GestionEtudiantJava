ba3a9adc2862a50b1398592db44e1335db54ac81
sergeabba <serge-mbaitadjim.abba@ism.edu.sn>
2021-01-14 00:16:28 +0000

    Java

---
+++ GestionEtudiant/src/cours/Liste/Exercice2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cours.Liste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author abbas
+ */
+public class Exercice2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> lPersonne=new ArrayList();
+        
+    }
+   
+    
+   
+}
